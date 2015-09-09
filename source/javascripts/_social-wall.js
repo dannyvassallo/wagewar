72842a0c6ab7f691b4f60e8c09533944b89073eb
dannyvassallo <danielvassallo87@gmail.com>
2015-09-09 16:39:20 -0400

    added social wall and fixed responsive header because iOS is terrible

---
+++ source/javascripts/_social-wall.js
@@ -0,0 +1,23 @@
+$(document).ready(function($){
+    $('#social-stream').dcSocialStream({
+        feeds: {
+            twitter: {
+                id: '#WAGEWAR, #FEARLESSRECORDS'
+            },
+            instagram: {
+                id: '#TWENTYONE, #FEARLESSRECORDS',
+                accessToken: '9803372.ad5b8ca.ad04165a1fbc4852b5f7d82abe8da3ba',
+                clientId: 'ad5b8cadaf80454698639bb22f59a0f9',
+                comments: 3,
+                likes: 10,
+            }
+        },
+        rotate: {
+            delay: 0
+        },
+        wall: true,
+        order: 'random',
+        iconPath: 'images/dcsns-dark/',
+        imagePath: 'images/dcsns-dark/'
+    });
+});
