ba98783d632dc8149d1de389b4b0512a65c77fe7
dannyvassallo <danielvassallo87@gmail.com>
2015-09-10 11:11:19 -0400

    updated hashtags

--- source/javascripts/_social-wall.js
+++ source/javascripts/_social-wall.js
@@ -2,10 +2,10 @@ $(document).ready(function($){
     $('#social-stream').dcSocialStream({
         feeds: {
             twitter: {
-                id: '#WAGEWAR, #FEARLESSRECORDS'
+                id: '#WAGEWAR, #FEARLESSRECORDS, #TWENTONE'
             },
             instagram: {
-                id: '#TWENTYONE, #FEARLESSRECORDS',
+                id: '#TWENTYONE, #FEARLESSRECORDS, #WAGEWAR',
                 accessToken: '9803372.ad5b8ca.ad04165a1fbc4852b5f7d82abe8da3ba',
                 clientId: 'ad5b8cadaf80454698639bb22f59a0f9',
                 comments: 3,
